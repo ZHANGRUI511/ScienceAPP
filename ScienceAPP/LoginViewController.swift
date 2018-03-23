2d241304f1404c9ab9d4597e2f2d5d7e243e78f4
Rui Zhang <zhagrui511@icloud.com>
2018-03-23 17:48:14 +1100

    sample

---
+++ ScienceAPP/LoginViewController.swift
@@ -0,0 +1,44 @@
+//
+//  LoginViewController.swift
+//  ScienceAPP
+//
+//  Created by 张睿 on 20/3/18.
+//  Copyright © 2018 张睿. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var login: UIButton!
+    @IBOutlet weak var StudentNum: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.login.layer.cornerRadius = 10
+        self.login.layer.borderWidth = 3
+        self.login.layer.borderColor = UIColor.black.cgColor
+        let singleWaterWaveView = waterview(frame:view.bounds)
+        view.addSubview(singleWaterWaveView)
+        view.sendSubview(toBack: singleWaterWaveView)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ScienceAPP/mainTabbar.swift
@@ -0,0 +1,35 @@
+//
+//  mainTabbar.swift
+//  ScienceAPP
+//
+//  Created by 张睿 on 20/3/18.
+//  Copyright © 2018 张睿. All rights reserved.
+//
+
+import UIKit
+
+class mainTabbar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tabBar.tintColor = UIColor.red
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
